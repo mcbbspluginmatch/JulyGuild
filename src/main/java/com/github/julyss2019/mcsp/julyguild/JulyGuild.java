b2739dbf407bbb98cae3d1e5f693156b12425a64
July_ss <july_ss_2016@outlook.com>
2019-06-16 13:05:11 +0800

    Initial commit

---
+++ src/main/java/com/github/julyss2019/mcsp/julyguild/JulyGuild.java
@@ -0,0 +1,11 @@
+package com.github.julyss2019.mcsp.julyguild;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class JulyGuild extends JavaPlugin {
+    public void onEnable() {
+
+    }
+
+    
+}
