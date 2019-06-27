8dc7fdefc702081976b6c17428b7cead6fbd52a3
July_ss <july_ss_2016@outlook.com>
2019-06-27 09:30:27 +0800

    update

--- src/main/java/com/github/julyss2019/mcsp/julyguild/gui/player/GuildManageGUI.java
+++ src/main/java/com/github/julyss2019/mcsp/julyguild/gui/player/GuildManageGUI.java
@@ -38,80 +38,81 @@ public class GuildManageGUI extends BaseGUI {
     public void build() {
         InventoryBuilder inventoryBuilder = new InventoryBuilder().row(3).title("&e&l宗门管理").colored();
 
-        inventoryBuilder.item(1, 2, new ItemBuilder()
-                .material(Material.PAINTING)
-                .displayName("&f设置公告")
-                .addLore("&7- 点击设置公告")
-                .addLore("")
-                .addLores(guild.getAnnouncements())
-                .colored()
-                .enchant(Enchantment.DURABILITY, 1)
-                .addItemFlag(ItemFlag.HIDE_ENCHANTS)
-                .build(), new ItemListener() {
-            @Override
-            public void onClicked(InventoryClickEvent event) {
-                close();
-                JulyMessage.sendColoredMessage(bukkitPlayer, "&e请在聊天栏输入并发送要设置的公告, 使用符号 &c" + settings.getGuildAnnouncementSplitChar() + " &e来换行, 最多支持 &c" + settings.getGuildAnnountcementMaxCount() + "行&e: ");
-
-                JulyChatFilter.registerChatFilter(bukkitPlayer, new ChatListener() {
-                    @Override
-                    public void onChat(AsyncPlayerChatEvent event) {
-                        event.setCancelled(true);
-
-                        String[] messages = event.getMessage().split(settings.getGuildAnnouncementSplitChar());
-
-                        if (messages.length > settings.getGuildAnnountcementMaxCount()) {
-                            JulyMessage.sendColoredMessage(bukkitPlayer, "&c公告最多能设置 &e" + settings.getGuildAnnountcementMaxCount() + "条&c, 使用分隔符 &e" + settings.getGuildAnnouncementSplitChar() + " &c换行.");
-                            return;
-                        }
-
-                        guild.setAnnouncements(Arrays.asList(messages));
-                        JulyMessage.sendColoredMessage(bukkitPlayer, "&d设置公告成功!");
-                        JulyChatFilter.unregisterChatFilter(bukkitPlayer);
-                    }
-                });
-            }
-        });
 
         inventoryBuilder
+                .item(1, 2, new ItemBuilder()
+                        .material(Material.PAINTING)
+                        .displayName("&f设置公告")
+                        .addLore("&7- 点击设置公告")
+                        .addLore("")
+                        .addLores(guild.getAnnouncements())
+                        .colored()
+                        .enchant(Enchantment.DURABILITY, 1)
+                        .addItemFlag(ItemFlag.HIDE_ENCHANTS)
+                        .build(), new ItemListener() {
+                    @Override
+                    public void onClicked(InventoryClickEvent event) {
+                        close();
+                        JulyMessage.sendColoredMessage(bukkitPlayer, "&e请在聊天栏输入并发送要设置的公告, 使用符号 &c" + settings.getGuildAnnouncementSplitChar() + " &e来换行, 最多支持 &c" + settings.getGuildAnnountcementMaxCount() + "行&e: ");
+
+                        JulyChatFilter.registerChatFilter(bukkitPlayer, new ChatListener() {
+                            @Override
+                            public void onChat(AsyncPlayerChatEvent event) {
+                                event.setCancelled(true);
+
+                                String[] messages = event.getMessage().split(settings.getGuildAnnouncementSplitChar());
+
+                                if (messages.length > settings.getGuildAnnountcementMaxCount()) {
+                                    JulyMessage.sendColoredMessage(bukkitPlayer, "&c公告最多能设置 &e" + settings.getGuildAnnountcementMaxCount() + "条&c, 使用分隔符 &e" + settings.getGuildAnnouncementSplitChar() + " &c换行.");
+                                    return;
+                                }
+
+                                guild.setAnnouncements(Arrays.asList(messages));
+                                JulyMessage.sendColoredMessage(bukkitPlayer, "&d设置公告成功!");
+                                JulyChatFilter.unregisterChatFilter(bukkitPlayer);
+                            }
+                        });
+                    }
+                })
+
                 .item(1, 3, new ItemBuilder()
-                .material(Material.BREWING_STAND_ITEM)
-                .displayName("&f成员管理").colored()
-                .enchant(Enchantment.DURABILITY, 1)
-                .addItemFlag(ItemFlag.HIDE_ENCHANTS)
-                .build())
+                        .material(Material.BREWING_STAND_ITEM)
+                        .displayName("&f成员管理").colored()
+                        .enchant(Enchantment.DURABILITY, 1)
+                        .addItemFlag(ItemFlag.HIDE_ENCHANTS)
+                        .build())
 
                 .item(1, 4, new ItemBuilder()
-                .material(Material.BLAZE_POWDER)
-                .displayName("&f宗门升级")
-                .enchant(Enchantment.DURABILITY, 1)
-                .addItemFlag(ItemFlag.HIDE_ENCHANTS)
-                .colored()
-                .build())
+                        .material(Material.BLAZE_POWDER)
+                        .displayName("&f宗门升级")
+                        .enchant(Enchantment.DURABILITY, 1)
+                        .addItemFlag(ItemFlag.HIDE_ENCHANTS)
+                        .colored()
+                        .build())
 
                 .item(1, 5, new ItemBuilder()
-                .material(Material.DOUBLE_PLANT)
-                .displayName("&f设置宗门图标")
-                .colored()
-                .enchant(Enchantment.DURABILITY, 1)
-                .addItemFlag(ItemFlag.HIDE_ENCHANTS)
-                .build())
+                        .material(Material.DOUBLE_PLANT)
+                        .displayName("&f设置宗门图标")
+                        .colored()
+                        .enchant(Enchantment.DURABILITY, 1)
+                        .addItemFlag(ItemFlag.HIDE_ENCHANTS)
+                        .build())
 
                 .item(1, 6, new ItemBuilder()
-                .material(Material.NAME_TAG)
-                .displayName("&f转让宗门")
-                .colored()
-                .enchant(Enchantment.DURABILITY, 1)
-                .addItemFlag(ItemFlag.HIDE_ENCHANTS)
-                .build())
+                        .material(Material.NAME_TAG)
+                        .displayName("&f转让宗门")
+                        .colored()
+                        .enchant(Enchantment.DURABILITY, 1)
+                        .addItemFlag(ItemFlag.HIDE_ENCHANTS)
+                        .build())
 
                 .item(1, 7, new ItemBuilder()
-                .material(Material.BARRIER)
-                .displayName("&c解散宗门")
-                .colored()
-                .enchant(Enchantment.DURABILITY, 1)
-                .addItemFlag(ItemFlag.HIDE_ENCHANTS)
-                .build(), new ItemListener() {
+                        .material(Material.BARRIER)
+                        .displayName("&c解散宗门")
+                        .colored()
+                        .enchant(Enchantment.DURABILITY, 1)
+                        .addItemFlag(ItemFlag.HIDE_ENCHANTS)
+                        .build(), new ItemListener() {
                     @Override
                     public void onClicked(InventoryClickEvent event) {
                         close();
